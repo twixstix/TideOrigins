d150059978b32d22170f931107dd1c3a695badeb
twixstix <ikazuchikumo@hotmail.com>
2022-07-26 20:27:34 -0700

    inital 1.18.2

---
+++ src/main/java/net/twixstixx/tideorigins/TideOriginsMod.java
@@ -0,0 +1,65 @@
+package net.twixstixx.tideorigins;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+//import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.MapColor;
+//import net.minecraft.block.FlowerBlock;
+import net.minecraft.block.Material;
+import net.minecraft.block.AbstractBlock.Settings;
+import net.minecraft.item.AliasedBlockItem;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
+import net.minecraft.util.registry.Registry;
+import net.twixstixx.tideorigins.block.EmberbloomBlock;
+import net.twixstixx.tideorigins.block.HeartFruitBushBlock;
+import net.twixstixx.tideorigins.item.CustomFood;
+import net.minecraft.entity.effect.StatusEffects;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TideOriginsMod implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger("tideorigins");
+	public static final String MOD_ID = "tideorigins";
+	//items
+	public static final Item PHOENIX_NECKLACE = new Item(new FabricItemSettings().group(ItemGroup.MISC).rarity(Rarity.UNCOMMON));
+	public static final Item JESTER_BATON = new Item(new FabricItemSettings().group(ItemGroup.MISC).rarity(Rarity.UNCOMMON));
+	//blocks
+	public static final Block EMBERBLOOM = new EmberbloomBlock(StatusEffects.FIRE_RESISTANCE, AbstractBlock.Settings.of(Material.PLANT).noCollision().breakInstantly().sounds(BlockSoundGroup.GRASS).luminance(state -> 7));
+	public static final Block FAIRYLIGHT = new Block(FabricBlockSettings.of(Material.SOLID_ORGANIC, MapColor.YELLOW).strength(1.0f).sounds(BlockSoundGroup.SHROOMLIGHT).nonOpaque().luminance(state -> 15));
+	public static final Block HEARTFRUIT_BUSH = new HeartFruitBushBlock(Settings.of(Material.PLANT).ticksRandomly().noCollision().sounds(BlockSoundGroup.SWEET_BERRY_BUSH));
+	//blockitems
+	public static final Item HEARTFRUIT = new AliasedBlockItem(HEARTFRUIT_BUSH, new FabricItemSettings().group(ItemGroup.FOOD).food(CustomFood.HEARTFRUIT));
+
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("TideOrigins loaded!");
+		CustomEntityActions.register();
+		//itemregistry
+		Registry.register(Registry.ITEM, new Identifier("tideorigins", "phoenix_necklace"), PHOENIX_NECKLACE);
+		Registry.register(Registry.ITEM, new Identifier("tideorigins", "jester_baton"), JESTER_BATON);
+		//blockregistry
+		Registry.register(Registry.BLOCK, new Identifier("tideorigins", "emberbloom"), EMBERBLOOM);
+		Registry.register(Registry.ITEM, new Identifier("tideorigins", "emberbloom"), new BlockItem(EMBERBLOOM, new FabricItemSettings().group(ItemGroup.MISC)));
+		Registry.register(Registry.BLOCK, new Identifier("tideorigins", "fairylight"), FAIRYLIGHT);
+		Registry.register(Registry.ITEM, new Identifier("tideorigins", "fairylight"), new BlockItem(FAIRYLIGHT, new FabricItemSettings().group(ItemGroup.MISC)));
+		Registry.register(Registry.BLOCK, new Identifier("tideorigins", "heartfruit_bush"), HEARTFRUIT_BUSH);
+		Registry.register(Registry.ITEM, new Identifier("tideorigins", "heartfruit"), HEARTFRUIT);
+	}
+}
---
+++ src/main/resources/data/tideorigins/functions/frostwalk.mcfunction
@@ -0,0 +1,2 @@
+fill ~-3 ~ ~-2 ~3 ~ ~2 minecraft:frosted_ice replace minecraft:water[level=0]
+fill ~-2 ~ ~-3 ~2 ~ ~3 minecraft:frosted_ice replace minecraft:water[level=0]
